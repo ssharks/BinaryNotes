decaedb59f829a126bfadf84f67b337643486e2d
Sjors Hettinga <sjors.hettinga@radarxense.com>
2025-05-22 10:34:28 +0200

    Add a script to generate ASN output for C# testing

---
+++ BinaryNotes.NET/BinaryNotesTests/test_asn_convert.py
@@ -0,0 +1,48 @@
+import zipfile
+import os
+import subprocess
+
+zip_path = '../../BNCompiler/target/bncompiler-1.6-dist.zip'
+test_asn_path = '../../BNCompiler/src/test/resources/test.asn'
+extract_to = 'bncompiler'
+
+os.makedirs(extract_to, exist_ok=True)
+
+path = os.path.dirname(os.path.abspath(__file__));
+
+print(path)
+
+with zipfile.ZipFile(os.path.join(path, zip_path), 'r') as zip_ref:
+    zip_ref.extractall(os.path.join(path,extract_to))
+
+java = "java"
+if os.environ.get('JAVA_HOME') != None :
+    java = os.path.join(os.environ.get('JAVA_HOME'),java)
+
+jar_path = os.path.join(path, extract_to, 'bncompiler-1.6.jar')
+namespace = "org.bn.coders.test_asn"
+
+# works with JAVA 20
+java_options = ['-Dsun.misc.URLClassPath.disableJarChecking=true',
+    '--add-opens', 'jdk.naming.rmi/com.sun.jndi.rmi.registry=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+    '--add-opens', 'java.base/sun.security.action=ALL-UNNAMED',
+    '--add-opens', 'java.base/sun.net=ALL-UNNAMED']
+
+cmd = [java]
+cmd.extend(java_options)
+
+# add the specific options for the bncompiler
+bn_compiler_args = [
+    '-classpath', jar_path,
+    'org.bn.compiler.Main',
+    '-m', 'cs',
+    '-ns', namespace, 
+    '-o', os.path.join(path, 'org/bn/coders/test_asn'),
+    "-f", os.path.join(path, test_asn_path)
+]
+cmd.extend(bn_compiler_args)
+
+result = subprocess.run(cmd, capture_output=True, text=True)
+print(result.stdout)
+print(result.stderr)
