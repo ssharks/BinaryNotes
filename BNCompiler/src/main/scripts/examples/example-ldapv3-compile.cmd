406c84fc577fca476e4086161444a65a4d5946f0
Sjors Hettinga <sjors.hettinga@radarxense.com>
2025-06-03 12:45:15 +0200

    Update the exmple scripts with the new command line working

--- BNCompiler/src/main/scripts/examples/example-ldapv3-compile.cmd
+++ BNCompiler/src/main/scripts/examples/example-ldapv3-compile.cmd
@@ -1,2 +1,2 @@
-call ../bncompiler.cmd -m java -o output/ldapv3 -ns ldapv3 -f ldapv3.asn
+call ../bncompiler.cmd -m java -o output/ldapv3 -ns ldapv3 ldapv3.asn
 javac -cp ../../../../JavaLibrary/target/binarynotes-1.6.jar output/ldapv3/*.java
